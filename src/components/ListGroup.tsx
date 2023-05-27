646ef832214a84caad88973782195c406482052b
MuhdHanish <muhammedhanish11@gmail.com>
2023-05-27 16:48:12 +0530

    created a event function using typescirpt

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,35 @@
+import {MouseEvent} from 'react';
+
 const ListGroup = () => {
+ const items = [
+  'NewYork',
+  'Los Angeles',
+  'Chicago',
+  'Houston',
+  'Phoenix',
+  'Philadelphia'
+ ]  
+ const handleClick = (event:MouseEvent) => {
+  const target = event.target as HTMLLIElement;
+    console.log(target.textContent);
+ }
   return (
     <>
-      <ul className="list-group">
-        <li className="list-group-item">Item 1</li>
-        <li className="list-group-item">Item 2</li>
-        <li className="list-group-item">Item 3</li>
-        <li className="list-group-item">Item 4</li>
+      <h1>List Group</h1>
+      { items.length!==0 &&
+       <ul className="list-group">
+        {
+          items.map((item, index) => {
+           return (
+            <li key={index} onClick={handleClick} className='list-group-items'>{item}</li>
+           )
+        })
+       }
       </ul>
+      }
+      {
+       items.length === 0 && <h3>No items found</h3>
+      }
     </>
   );
 };
